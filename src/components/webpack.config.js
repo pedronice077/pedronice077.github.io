59aa087b026a9f9a34e03eb823af1baf08cd4a54
pedrolove <nicepedro00@gmail.com>
2025-04-10 03:59:36 +0100

    Update webpack.config.js

--- src/components/webpack.config.js
+++ src/components/webpack.config.js
@@ -1,6 +1,104 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development', // or 'production'
+  ...
+};
+devtool: 'source-map', // Add this line
+  output: {
+  filename: 'bundle.[contenthash].js',
+  path: path.resolve(__dirname, 'dist')
+},
+module: {
+  rules: [
+    {
+      test: /\.tsx?$/,
+      use: 'ts-loader',
+      exclude: /node_modules/
+    },
+    {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    },
+    {
+      test: /\.(png|jpg|jpeg|gif|svg)$/,
+      type: 'asset/resource'
+    }
+  ]
+},
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  ...
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
+};
+devServer: {
+  static: path.join(__dirname, 'dist'),
+  compress: true,
+  port: 9000,
+  hot: true, // Enable Hot Module Replacement
+  open: true // Automatically open the browser
+}
+optimization: {
+  splitChunks: {
+    chunks: 'all'
+  }
+}
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development', // Change to 'production' for production builds
+  entry: './src/index.tsx',
+  output: {
+    filename: 'bundle.[contenthash].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/,
+        type: 'asset/resource'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+    hot: true,
+    open: true
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    }
+  }
+};
+
+module.exports = {
   entry: './src/index.tsx',
   output: {
     filename: 'bundle.js',
